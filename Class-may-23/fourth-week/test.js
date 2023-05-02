79a87eb5caaa2ff6323dded8398bd4cf45af40d5
Jorge Gomez <andispotlight@gmail.com>
2023-05-02 20:50:27 +0200

    commint adding js info

---
+++ Class-may-23/fourth-week/test.js
@@ -0,0 +1,20 @@
+const numero = [10, 40, 44, 33, 45]
+
+//function sumadeArrayDeNums(arrayDeNumero) {};
+
+
+const sumadeArrayDeNumsFlecha = (arrayDeNumero) => {
+    let sumatotal = 0; 
+for (let i = 0; i < arrayDeNumero.length; i++){ sumatotal += arrayDeNumero[i]
+    }
+    return sumatotal;
+};
+{ sumadeArrayDeNumsFlecha(numero) [console.log(numero[i])];}
+
+// const sumadeArrayDeNumsFlecha = (arrayDeNumero) => {
+//     let sumatotal = 0; 
+//     for (let i = 0, i < arrayDeNumero.length; i++){ sumatotal += arrayDeNumero[i]
+//     }
+//     return sumatotal
+// };
+// sumadeArrayDeNumsFlecha(numeros)
