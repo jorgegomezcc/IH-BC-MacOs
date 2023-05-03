54bbdaa0f10380619410ba84dd1e3e1157338aa2
Jorge Gomez <andispotlight@gmail.com>
2023-05-03 23:03:34 +0200

    commint adding css prueba personal

---
+++ Class-April-23/Third_week/Lab js/index.js
@@ -0,0 +1,18 @@
+var hacker1 = "Jorge"
+var hacker2 = "Eduardo"
+
+// Iteración 1: Nombres y entradas
+
+console.log("El nombre del conductor es "+ (hacker1));
+console.log("El nombre del navegante es " + (hacker2));
+
+// Iteración 2: Condicionales
+
+if (hacker1.length > hacker2.length) { 
+    console.log(hacker1 + " tiene el nombre mas largo que " + hacker2);
+} else { 
+    console.log(hacker2 + " tiene el nombre mas largo que " + hacker1);
+}
+
+// Iteración 3: Loops
+
