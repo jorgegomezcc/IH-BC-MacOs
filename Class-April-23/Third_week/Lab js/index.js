1ce00f980df8fca7dadb5bf3782d1cea63352982
Jorge Gomez <andispotlight@gmail.com>
2023-05-04 23:59:48 +0200

    commit7 adding js lab

--- Class-April-23/Third_week/Lab js/index.js
+++ Class-April-23/Third_week/Lab js/index.js
@@ -28,7 +28,42 @@ if (hacker1.length > hacker2.length) {
     console.log(`Vaya ${hacker1} y ${hacker2} tienen la misma longitud`)
 }
 
-// Iteración 3: Loops
+// Iteración 3: Loops  // hacker1 es la variable "var hacker1 = Jorge "
+
+let deletreado = "";
+
+let mayusReverse = hacker1.split("");
+mayusReverse.reverse("");
+
+// Mayusculas + deletrado 
+
+ for (let index = 0; index < hacker1.length; index++) {
+     deletreado += hacker1[index].toUpperCase() + " ";
+ }
+console.log(deletreado) 
+
+// Mayusculas + deletreado + inverso "dificil"
+
+let deletradoReverse= "";
+
+for (let index = 0; index < mayusReverse.length; index++) {
+    deletradoReverse += mayusReverse[index].toUpperCase() + " ";
+}
+
+console.log(deletradoReverse);
+
+// Mayusculas + deletreado + inverso "facil"
+
+let deletreadoReverse1= "";
+
+for (let index = hacker1.length -1; index >= 0 ; index--) {
+    deletreadoReverse1 += hacker1[index].toUpperCase() + " ";
+}
+
+
+
+console.log(deletreadoReverse1);
+
 
 
 
