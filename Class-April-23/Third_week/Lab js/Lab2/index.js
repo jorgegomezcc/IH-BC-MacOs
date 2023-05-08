95507575991ce6c6cd271710fb3ba3060154a728
Jorge Gomez <andispotlight@gmail.com>
2023-05-08 16:24:26 +0200

    commit8 adding js lab

---
+++ Class-April-23/Third_week/Lab js/Lab2/index.js
@@ -0,0 +1,133 @@
+// let a = 11
+// let b = 12
+
+// Numero mas alto
+//manera condicional
+
+console.log("numero mas alto");
+console.log("---------------------");
+
+function maxOfTwonumeros(a, b) {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
+     }
+   };
+
+   console.log(maxOfTwonumeros (8, 10))
+
+//   Metodo de numeros - Metodo MATH
+
+const maxOfTwonumerosMath = (a, b) => {
+    return Math.max(a, b)
+};
+console.log(maxOfTwonumerosMath (14, 16));
+
+
+
+//   palabra mas larga
+
+console.log("Palabra mas larga");
+console.log("----------------------");
+
+const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+
+function findLongestWord(words) {
+  let longestWord = "";
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord(words)); // Output: 'crocodile'
+
+// teoria
+
+//   function findLongestWord(words) {
+//     let longestWord = "";
+//     for (let i = 0; i < words.length; i++) {
+//       if (words[i].length > longestWord.length) {
+//         longestWord = words[i];
+//       }
+//     }
+//     return longestWord;
+//   }
+
+//   Calcular suma
+
+console.log("Calcular Suma");
+console.log("---------------------");
+
+  const numeros = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+  function sumnumeros(numeros) {
+    let sum = 0;
+    for (let i = 0; i < numeros.length; i++) {
+      sum += numeros[i];
+    }
+    return sum;
+  }
+  console.log(sumnumeros(numeros)); // Output: 87
+
+
+  const mixedArr = [6, "miami", 1, true, "barca", "200", "Lisboa", 8, 10];
+
+  const suma = (matrixMixta) => {  //funcion tipo flecha
+    if (matrixMixta.length === 0) return 0 //comprobamos si tiene contenido 
+    let suma = 0 
+    for (let i = 0; i < matrixMixta.length; i++) {  //Creamos un bucle para comprobar el polimorfismo
+        let elemento = matrixMixta[i]; //Guardamos la iteracion individual en una variable
+        if (typeof elemento === 'String') { //condicionamos el tipo de elemento string para tranformarlo en numero
+            suma += elemento.length
+        }else if ( typeof elemento === true) { // en esta condicion transformamos el tipo de dato buleano true en un numero
+            suma += 1
+        }else if (typeof elemento === 'object' && valor !== null) { // En esta condicion hacemos que si se incluye un objeto devolvemos un error (añadimos el null por que en esencia el valor null tambien es un objeto)
+            throw new Error("No aceptamos objetos dentro de esta operacion")
+        }else { //finalmente equiparamos la variable "elemento" a la variable "suma" para en efecto realizar la operacion matematica
+            elemento = suma
+        }
+    }
+
+  }
+
+//   typeof === constructor de verificacion de tipos de datos
+
+
+// Calcular media
+
+console.log("Calcular media");
+console.log("------------------");
+
+const numeros1 = [2, 6, 9, 10, 7, 4, 1, 9]
+
+  function mediadenumeros(numeros) {
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+      suma += numeros[i];
+    }
+    return suma / numeros.length;
+  };
+  console.log(mediadenumeros(numeros1)); // 
+
+
+  const palabras1 = ['crab','poison','contagious','simple','bring','sharp','playground','poison','communion','simple','bring'];
+
+  const arrayUnificacion = (palabras) => {
+    if (palabras.length === 0) return null;
+    let arraylimpio = [];
+
+    palabras.forEach(elemento); 
+        for (let i = 0; i < palabras.length; i ++) {
+        if (!arraylimpio.includes(elemento)) {
+            arraylimpio.push(elemento);
+        }
+        console.log(arraylimpio)
+        return arraylimpio;
+    };
+  }
+console.log(arrayUnificacion);
---
+++ Class-may-23/fourth-week/javescript/CFU/1/cfu1.js
@@ -0,0 +1,62 @@
+// CFU JS 1
+// con if
+
+
+let nombre = prompt("Cúal es tu nombre: ")
+let idioma = prompt("Dime tu idioma / Tell me your languaje / Dis moi ta langue")
+
+// if (idioma === "Español" || idioma === "español" || idioma === "espanol") {
+//     console.log(`Hola ${nombre}, bienvenid@`);
+// } else if ( idioma === "Frances" || idioma === "frances" || idioma === "francés" || idioma === "Francés" || idioma === "French" || idioma === "french") {
+//     console.log(`Bonjour ${nombre} bienvenue`)
+// } else if (idioma === "Inglés" || idioma=== "inglés" || idioma === "ingles" || idioma === "Ingles") {
+//     console.log(`Hi ${nombre}, welcome`);
+// }else {
+//     console.log("Idioma no reconocido / Language not recognized")
+// }
+
+// con switch
+
+
+switch (idioma) {
+    case value: Español
+        console.log(`Hola ${nombre}, bienvenid@`)
+        break;
+    case value: español
+        console.log(`Hola ${nombre}, bienvenid@`)
+        break;
+    case value: Espanol
+        console.log(`Hola ${nombre}, bienvenid@`)
+        break;
+    case value: espanol
+        console.log(`Hola ${nombre}, bienvenid@`)
+        break;
+    case value: Francés
+        console.log(`Bonjour ${nombre}, bienveniue`);
+        break;    case value: Francés
+        console.log(`Bonjour ${nombre}, bienveniue`);
+        break;    
+    case value: francés
+        console.log(`Bonjour ${nombre}, bienveniue`);
+        break;
+    case value: Frances
+        console.log(`Bonjour ${nombre}, bienveniue`);
+        break;    
+    case value: erances
+        console.log(`Bonjour ${nombre}, bienveniue`);
+        break;    
+    case value: Inglés
+        console.log(`Hi ${nombre}, Welcome`);
+        break;
+    case value: Inglés
+        console.log(`Hi ${nombre}, Welcome`);
+        break;
+    case value: Inglés
+        console.log(`Hi ${nombre}, Welcome`);
+        break;
+    case value: Inglés
+        console.log(`Hi ${nombre}, Welcome`);
+        break;
+    default:
+        break;
+}
